3bf25ee1cc552620b78ec5f008fad43d7c247286
Mike Whaley <michael.d.whaley@gmail.com>
2023-02-18 08:30:24 -0500

    re attempt at deployment

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:4000/')
+    driver.get('http://localhost:4001/')
 })
 
 afterAll(async () => {
--- server.js
+++ server.js
@@ -90,6 +90,6 @@ app.get("/api/player", (req, res) => {
   }
 });
 
-app.listen(4000, () => {
-  console.log(`Listening on 4000`);
+app.listen(4001, () => {
+  console.log(`Listening on 4001`);
 });
